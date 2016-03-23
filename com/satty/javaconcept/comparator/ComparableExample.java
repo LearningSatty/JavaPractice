0955a9886fb74ec4d0be5144ef1e9c94951c8992
LearningSatty <satty.ads@gmail.com>
2016-03-23 12:11:33 +0530

    Create ComparableExample.java

---
+++ com/satty/javaconcept/comparator/ComparableExample.java
@@ -0,0 +1,12 @@
+package com.satty.javaconcept.comparator;
+
+public class ComparableExample implements Comparable<Employee>
+{
+
+	@Override
+	public int compareTo(Employee o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
