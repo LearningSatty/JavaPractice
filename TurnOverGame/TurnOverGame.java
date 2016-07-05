2d1967d394141e4e7fd7b5352f421f64b3e9645f
LearningSatty <satty.ads@gmail.com>
2016-07-05 17:19:31 +0530

    Update TurnOverGame.java

--- TurnOverGame/TurnOverGame.java
+++ TurnOverGame/TurnOverGame.java
@@ -6,22 +6,10 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class TurnOverGame {
 
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int a = 60;
-//		System.out.println(a<<1);
-//		System.out.println(a>>1);
-//		System.out.println((a+1)&1);
-//		String s ="WXYX";
-//		System.out.println((int)s.charAt(0));
-//		char ch = (char) (s.charAt(0) ^ ' ');
-//		System.out.println(ch);
-//		
-//		int array[]={3,4,1,3,1,7,2,2,4};
-//		int remaining=0;
-//		for(int i=0;i < array.length;i++ ) 
-//		     remaining^=array[i]; 
-//		 System.out.println(remaining);
 		
 		Scanner sc = new Scanner(System.in);
 		int T_Case = sc.nextInt();
@@ -39,11 +27,7 @@ public class TurnOverGame {
 				}
 			}
 			
-			for (int j = 0; j < arr.length; j++) {
-				for (int k = 0; k < arr.length; k++) {
-					callBFS(arr, j, k);
-				}
-			}
+		 callBackTrack(arr, 0, 0);
 			
 			System.out.println("Case #"+ (i+1));
 			if(t_cal==Integer.MAX_VALUE)
@@ -58,38 +42,48 @@ public class TurnOverGame {
 		}
 	}
 	
-	private static int t_cal;
-	
-	private static  void callBFS(int arr[][],int j,int k){
-		int arr_vis[][] = new int[arr.length][arr.length];
-		
-		Queue<GameData> queue = new ArrayBlockingQueue<GameData>(1000);
-		arr_vis[j][k] = 1;
-		queue.add(new GameData(j, k, arr[j][k]));
-		int cal=0;
-		while(!queue.isEmpty()){
-			if(checkForSymmetry(arr)){
-				if(cal<t_cal)
-					t_cal =  cal;
-				break;
+	private static void callBackTrack(int[][] arr, int x, int cal) {
+		// TODO Auto-generated method stub
+		int j, k;
+		if(checkForSymmetry(arr)){
+			if(cal<t_cal)
+			{
+				t_cal=cal;
+				
 			}
-			else{
-				GameData d = queue.remove();
-				j = d.i;
-				k = d.j;
-				int x[] = {0, 1, -1, 0};
-				int y[] = {1, 0, 0, -1};
-				for (int s = 0; s < y.length; s++) {
-					if(j+x[s]>=0 && j+x[s]<arr.length &&  k+y[s]>=0 && k+y[s]<arr.length && arr_vis[j+x[s]][k+y[s]]!=1){
-						arr[j+x[s]][k+y[s]] = arr[j+x[s]][k+y[s]]==0?1:0; 
-						arr_vis[j+x[s]][k+y[s]] =1;
-						queue.add(new GameData(j+x[s], k+y[s], arr[j+x[s]][k+y[s]]));
-					}
-				}
+			return;
+		}
+		if (x >= 16)
+	        return ;
+
+	    j = x/4;
+	    k = x%4;
+	    
+	    callBackTrack(arr, x + 1, cal);
+	    arr[j][k] = (arr[j][k]==0)?1:0;
+	    int x1[] = {0, 1, -1, 0};
+		int y1[] = {1, 0, 0, -1};
+		for (int s = 0; s < y1.length; s++) {
+			if(j+x1[s]>=0 && j+x1[s]<arr.length &&  k+y1[s]>=0 && k+y1[s]<arr.length){
+				arr[j+x1[s]][k+y1[s]] = (arr[j+x1[s]][k+y1[s]]==0)?1:0; 
 			}
 		}
+	    
+		callBackTrack(arr, x+1, cal+1);
+		arr[j][k] = (arr[j][k]==0)?1:0;
+		int x2[] = {0, 1, -1, 0};
+		int y2[] = {1, 0, 0, -1};
+		for (int s = 0; s < y2.length; s++) {
+			if(j+x2[s]>=0 && j+x2[s]<arr.length &&  k+y2[s]>=0 && k+y2[s]<arr.length ){
+				arr[j+x2[s]][k+y2[s]] = (arr[j+x2[s]][k+y2[s]]==0)?1:0; 
+			}
+		}
+		
+		return;
 	}
 
+	private static int t_cal;
+	
 	private static boolean checkForSymmetry(int[][] arr) {
 		// TODO Auto-generated method stub
 		int i = arr[0][0];
@@ -104,17 +98,6 @@ public class TurnOverGame {
 		return true;
 	}
 
+
 }
 
-class GameData{
-	int i, j;
-	int value;
-
-	public  GameData(int i, int j, int value) {
-		super();
-		this.i = i;
-		this.j = j;
-		this.value = value;
-	}
-	
-}
